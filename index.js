e1741da19705ee9ff5c376c3f91b2218f406486f
Diego Torres <diegotorres@Diegos-MacBook-Air-2.local>
2020-12-17 14:47:07 -0800

    Website content

---
+++ index.js
@@ -0,0 +1,68 @@
+/**
+ * Name: _your name here_
+ * Date: _add date here_
+ * Section: CSE 154 _your section here_
+ *
+ * -- your description of what this file does here --
+ * Do not keep comments from this template in any work you submit (functions included under "Helper
+ * functions" are an exception, you may keep the function names/comments of id/qs/qsa/gen)
+ */
+"use strict";
+
+(function() {
+
+  /**
+   * Add a function that will be called when the window is loaded.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * CHANGE: Describe what your init function does here.
+   */
+  function init() {
+    // THIS IS THE CODE THAT WILL BE EXECUTED ONCE THE WEBPAGE LOADS
+  }
+
+  /** ------------------------------ Helper Functions  ------------------------------ */
+  /**
+   * Note: You may use these in your code, but remember that your code should not have
+   * unused functions. Remove this comment in your own code.
+   */
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} selector - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+})();
